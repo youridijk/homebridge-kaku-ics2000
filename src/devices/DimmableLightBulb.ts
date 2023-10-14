e3f6175c43d225a7ea00e3d4bf859ca7c4c88099
youridijk <steltje.maraboe_0q@icloud.com>
2023-10-14 15:41:14 +0200

    #28 Fix issue with wrong brightness levels in iOS 17

--- src/devices/DimmableLightBulb.ts
+++ src/devices/DimmableLightBulb.ts
@@ -5,6 +5,7 @@ import {DimDevice} from 'ics-2000';
 
 export default class DimmableLightBulb extends LightBulb {
   public readonly device: DimDevice;
+  private readonly maxBrightness: number;
 
   constructor(
     platform: KAKUPlatform,
@@ -16,22 +17,26 @@ export default class DimmableLightBulb extends LightBulb {
     this.service.getCharacteristic(this.platform.Characteristic.Brightness)
       .onGet(this.getBrightness.bind(this))
       .onSet(this.changeBrightness.bind(this))
-      // KAKU uses a value from 0 to 255 for the dim value, so we set this as the min en max values
+    // KAKU uses a value from 0 to 255 for the dim value, so we set this as the min en max values
 
-      .setProps({
-        minValue: 0,
-        maxValue: this.device.deviceConfig.maxBrightness ?? 255,
-      });
+    // .setProps({
+    //   minValue: 0,
+    //   maxValue: this.device.deviceConfig.maxBrightness ?? 255,
+    // })
+    ;
+    this.maxBrightness = this.device.deviceConfig.maxBrightness ?? 255;
+  }
+
+  private convertToRange(value: number, currentMax: number, newMax: number) {
+    return value / currentMax * newMax;
   }
 
   private async getBrightness(): Promise<number> {
     try {
-      const status = await this.device.getDimLevel();
-      if (status == null) {
-        this.logger.debug('status for ' + this.deviceName + ' ' + await this.device.getStatus());
-      }
-      this.logger.debug(`Current brightness for ${this.deviceName}: ${status}`);
-      return status;
+      const rawDimLevel = await this.device.getDimLevel();
+      const dimLevel = this.convertToRange(rawDimLevel, this.maxBrightness, 100);
+      this.logger.debug(`Current brightness for ${this.deviceName}: raw=${rawDimLevel}, range=${dimLevel}`);
+      return dimLevel;
     } catch (e) {
       this.logger.error(`Error getting brightness for ${this.deviceName}: ${e}`);
       throw new this.platform.api.hap.HapStatusError(HAPStatus.SERVICE_COMMUNICATION_FAILURE);
@@ -40,9 +45,10 @@ export default class DimmableLightBulb extends LightBulb {
 
   private async changeBrightness(newValue: CharacteristicValue): Promise<void> {
     try {
-      this.logger.debug(`Brightness changed: ${newValue}`);
-      // await this.hub.dimDevice(this.deviceId, 4, newValue as number, this.isGroup, true);
-      await this.device.dim(newValue as number, true);
+      let newDimLevel = this.convertToRange(newValue as number, 100, this.maxBrightness);
+      newDimLevel = Math.round(newDimLevel);
+      this.logger.debug(`Brightness changed: ${newDimLevel}`);
+      await this.device.dim(newDimLevel, true);
     } catch (e) {
       this.logger.error(`Error changing brightness for ${this.deviceName}: ${e}`);
       throw new this.platform.api.hap.HapStatusError(HAPStatus.SERVICE_COMMUNICATION_FAILURE);
